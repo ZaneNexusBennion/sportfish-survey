94badec2e0f01205ce131527d261d4ee5198da8d
zanebennion <zanebennion@zben.me>
2026-07-13 09:28:01 -0800

    created form with text input

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-      <div className="text-9xl m-auto">Hello</div>
+      <div className="m-auto">
+          <div className="m-auto flex flex-col">
+            <div className="text-9xl">FISH</div>
+            <form className="flex flex-col gap-1">
+                <input type="text" className="border border-black rounded-sm"/>
+                <button className="px-2 py-1 rounded-sm bg-gray-300 w-min">Submit</button>
+            </form>
+          </div>
+      </div>
   );
 }

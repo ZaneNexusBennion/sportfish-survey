4a192f9bde98200d1d2216f8d8c61ee6aba91811
zanebennion <zanebennion@zben.me>
2026-07-13 10:17:33 -0800

    printing input to server

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
+'use client'
+
+import { useState } from "react";
+import { printServer } from "@/lib/printServer";
+
 export default function Home() {
-  return (
-      <div className="m-auto">
-          <div className="m-auto flex flex-col">
-            <div className="text-9xl">FISH</div>
-            <form className="flex flex-col gap-1">
-                <input type="text" className="border border-black rounded-sm"/>
-                <button className="px-2 py-1 rounded-sm bg-gray-300 w-min">Submit</button>
-            </form>
-          </div>
-      </div>
-  );
+    const [input, setInput] = useState("")
+
+    function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        printServer(input);
+
+        setInput("");
+    }
+
+    return (
+        <div className="m-auto">
+            <div className="m-auto flex flex-col">
+                <div className="text-9xl">FISH</div>
+                <form className="flex flex-col gap-1" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="border border-black rounded-sm"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button className="px-2 py-1 rounded-sm bg-gray-300 w-min">Submit</button>
+                </form>
+            </div>
+        </div>
+    );
 }
---
+++ lib/printServer.ts
@@ -0,0 +1,5 @@
+'use server'
+
+export async function printServer(msg: string) {
+    console.log(msg)
+}
